e05d2228a088afd48c3433aa96d7a8c78c9688d5
ashd0wn <pheme@ashd0wn.dev>
2026-05-18 08:33:55 +0200

    Fix: liquidsoap 2.4.1 noble, centrifugo via GitHub release

--- stations/setup/liquidsoap.sh
+++ stations/setup/liquidsoap.sh
@@ -2,30 +2,35 @@
 
 # Packages required by Liquidsoap and Audio Post-processing
 PACKAGES=(
-    libao4 libfaad2 libfdk-aac2 libgd3 liblo7 libmad0 libmagic1 
-    libportaudio2 libsdl2-image-2.0-0 libsdl2-ttf-2.0-0 libsoundtouch1 libxpm4 
-    libasound2-plugins libasound2-data libavcodec60 libavdevice60 libavfilter9 
-    libavformat60 libavutil58 libpulse0 libsamplerate0 libswresample4 libswscale7 
+    libao4 libfaad2 libfdk-aac2 libgd3 liblo7 libmad0 libmagic1
+    libportaudio2 libsdl2-image-2.0-0 libsdl2-ttf-2.0-0 libsoundtouch1 libxpm4
+    libasound2-plugins libasound2-data libavcodec60 libavdevice60 libavfilter9
+    libavformat60 libavutil58 libpulse0 libsamplerate0 libswresample4 libswscale7
     libtag1v5 libsrt1.5-openssl bubblewrap ffmpeg liblilv-0-0 libjemalloc2 ladspa-sdk
 )
 
 apt_get_with_lock update
 apt_get_with_lock install -y --no-install-recommends "${PACKAGES[@]}"
 
-# Download and install Liquidsoap
+# Détecter la version Ubuntu pour choisir le bon package
+UBUNTU_CODENAME=$(lsb_release -cs 2>/dev/null || echo "noble")
 ARCHITECTURE=$(dpkg --print-architecture | awk -F- '{ print $NF }')
-LIQUIDSOAP_DEB_URL="https://github.com/savonet/liquidsoap/releases/download/v2.2.3/liquidsoap_2.2.3-ubuntu-jammy-1_${ARCHITECTURE}.deb"
-wget -O liquidsoap.deb "$LIQUIDSOAP_DEB_URL"
-dpkg -i liquidsoap.deb
 
-# Install any missing dependencies
+LIQUIDSOAP_VERSION="2.4.1"
+LIQUIDSOAP_DEB_URL="https://github.com/savonet/liquidsoap/releases/download/v${LIQUIDSOAP_VERSION}/liquidsoap_${LIQUIDSOAP_VERSION}-ubuntu-${UBUNTU_CODENAME}-ocaml5.4.0-1_${ARCHITECTURE}.deb"
+
+echo "Téléchargement Liquidsoap ${LIQUIDSOAP_VERSION} pour ${UBUNTU_CODENAME}/${ARCHITECTURE}..."
+wget -O liquidsoap.deb "$LIQUIDSOAP_DEB_URL"
+
+if [ $? -ne 0 ]; then
+    echo "Échec du téléchargement pour ${UBUNTU_CODENAME}, tentative avec noble..."
+    LIQUIDSOAP_DEB_URL="https://github.com/savonet/liquidsoap/releases/download/v${LIQUIDSOAP_VERSION}/liquidsoap_${LIQUIDSOAP_VERSION}-ubuntu-noble-ocaml5.4.0-1_${ARCHITECTURE}.deb"
+    wget -O liquidsoap.deb "$LIQUIDSOAP_DEB_URL"
+fi
+
+dpkg -i liquidsoap.deb
 apt_get_with_lock install -y -f --no-install-recommends
 
-# Create a symbolic link for liquidsoap
-ln -s /usr/bin/liquidsoap /usr/local/bin/liquidsoap
-
-# Clean up
+ln -sf /usr/bin/liquidsoap /usr/local/bin/liquidsoap
 rm -f liquidsoap.deb
-
-# Go back to the installer home directory
 cd "$installerHome"
--- web/setup/centrifugo.sh
+++ web/setup/centrifugo.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
-# Install the Golang package without recommended packages
-apt_get_with_lock install -y --no-install-recommends golang
+CENTRIFUGO_VERSION="5.4.8"
+ARCHITECTURE=$(dpkg --print-architecture | awk -F- '{ print $NF }')
 
-# Install a specific version of Centrifugo using Go
-go install github.com/centrifugal/centrifugo/v4@d465b5932ab786273f081392e1dc8fdfd2d2ec10
+# Mapper l'architecture Debian vers celle de GitHub releases
+case "$ARCHITECTURE" in
+    amd64) ARCH="amd64" ;;
+    arm64) ARCH="arm64" ;;
+    *)     ARCH="amd64" ;;
+esac
 
-# Move the centrifugo binary to /usr/local/bin
-mv /root/go/bin/centrifugo /usr/local/bin/centrifugo
+echo "Téléchargement Centrifugo ${CENTRIFUGO_VERSION}..."
+CENTRIFUGO_URL="https://github.com/centrifugal/centrifugo/releases/download/v${CENTRIFUGO_VERSION}/centrifugo_${CENTRIFUGO_VERSION}_linux_${ARCH}.tar.gz"
 
-# Remove the Go installation directory
-rm -rf /root/go
+wget -O /tmp/centrifugo.tar.gz "$CENTRIFUGO_URL"
+tar -xzf /tmp/centrifugo.tar.gz -C /tmp centrifugo
+mv /tmp/centrifugo /usr/local/bin/centrifugo
+chmod +x /usr/local/bin/centrifugo
+rm -f /tmp/centrifugo.tar.gz
 
-# Copy the Centrifugo configuration file to the appropriate directory
+# Vérifier
+centrifugo version
+
+# Copier la config
 cp web/centrifugo/config.json /var/pheme/centrifugo/config.json
