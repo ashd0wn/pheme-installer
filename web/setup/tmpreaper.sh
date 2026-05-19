feb1367ddf5efa4e87c1d12c12076bb29a6702d1
ashd0wn <pheme@ashd0wn.dev>
2026-05-19 07:36:59 +0200

    Fix: centrifugo absolute path in supervisor, tmpreaper rollback

--- web/setup/tmpreaper.sh
+++ web/setup/tmpreaper.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-# Pré-accepter la configuration debconf de tmpreaper
-# pour éviter l'interaction utilisateur pendant l'installation
-echo "tmpreaper tmpreaper/readsecurity boolean true" | debconf-set-selections
-echo "tmpreaper tmpreaper/readsecurity_upgrading boolean true" | debconf-set-selections
-
-DEBIAN_FRONTEND=noninteractive apt_get_with_lock install -y --no-install-recommends tmpreaper
+apt_get_with_lock install -y --no-install-recommends tmpreaper
